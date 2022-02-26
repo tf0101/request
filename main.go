71a6045cc85e536b6db62f2c598ea7389ef39262
tf0101 <tf0101_sh@icloud.com>
2022-02-27 01:15:30 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package request
+
+import(
+	"time"
+    "net/http"
+)
+
+type Request_paramater struct{
+	Header *Header_list
+}
+
+type Header_list struct{
+	Header_json *map[string]string
+}
+
+func request(url string, rp *Request_paramater)(*http.Response,error){
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil,err
+	}
+
+	//set header
+	if rp.Header != nil{
+		for key,value := range *rp.Header.Header_json{
+			req.Header.Set(key,value)
+		}
+	}
+
+	//timeout 90 second
+	timeout := time.Duration(90 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+    resp, err := client.Do(req)
+	if err != nil {
+		return nil,err
+	}
+
+	return resp,err
+}
+
+func request_post(url string, rp *Request_paramater)(*http.Response,error){
+
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return nil,err
+	}
+
+	//set header
+	if rp.Header != nil{
+		for key,value := range *rp.Header.Header_json{
+			req.Header.Set(key,value)
+		}
+	}
+
+	//timeout 90 second
+	timeout := time.Duration(90 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+    resp, err := client.Do(req)
+	if err != nil {
+		return nil,err
+	}
+
+	return resp,err
+}
